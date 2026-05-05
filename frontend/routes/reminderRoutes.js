244868628f8c530ca26553bfccb84dd6af1e8de0
ManojKumarMotakatla <mightymanoj9@gmail.com>
2026-05-05 13:45:16 +0530

    Delete frontend directory

--- frontend/routes/reminderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router  = express.Router();
-const { uploadReport, getReports, deleteReport } = require("../controllers/reportController");
-
-router.post("/upload",       uploadReport);
-router.get("/get/:user_id",  getReports);
-router.delete("/delete/:id", deleteReport);
-
-module.exports = router;
--- frontend/routes/symptomRoutes.js
+++
@@ -1,8 +0,0 @@
-const express   = require("express");
-const router    = express.Router();
-const { saveSymptoms, getSymptoms } = require("../controllers/symptomController");
-
-router.post("/save",          saveSymptoms);
-router.get("/history/:user_id", getSymptoms);
-
-module.exports = router;
