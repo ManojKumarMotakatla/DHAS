65e2631af0e89c84722c38a98478683fd952be1d
Vyshu <kesuvyshnavi22@gmail.com>
2026-05-15 16:23:15 +0530

    fixed reminder saving variable

--- Backend/controllers/reminderController.js
+++ Backend/controllers/reminderController.js
@@ -40,7 +40,7 @@ const addReminder = (req, res) => {
 
     const sql = `
         INSERT INTO reminders
-          (user_id, medicine_name,
+          (user_id, medicinename,
            schedule_type, schedule_label,
            dose_count, doses_label,
            times, days, month_day,
