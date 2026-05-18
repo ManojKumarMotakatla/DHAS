ce5b7879d9a49492c72ae7548b962edc063dd526
Vyshu <kesuvyshnavi22@gmail.com>
2026-05-18 09:56:31 +0530

    modifying db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,20 +4,27 @@ console.log("MYSQLHOST:", process.env.MYSQLHOST);
 console.log("MYSQLUSER:", process.env.MYSQLUSER);
 console.log("MYSQLDATABASE:", process.env.MYSQLDATABASE);
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 30000
 });
 
-db.connect((err) => {
+// Test connection on startup
+pool.getConnection((err, connection) => {
   if (err) {
     console.log("Database connection failed:", err);
   } else {
     console.log("Connected to MySQL database");
+    connection.release();
   }
 });
 
-module.exports = db;
+module.exports = pool;
