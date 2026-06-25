33b21996001a987ad8471b8831665786c1af6022
ManojKumarMotakatla <mightymanoj9@gmail.com>
2026-06-25 19:09:26 +0530

    Delete Backend directory

--- Backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express  = require("express");
-const router   = express.Router();
-const { register, login, googleAuth } = require("../controllers/authController");
-
-router.post("/register",    register);
-router.post("/login",       login);
-router.post("/auth/google", googleAuth);
-
-module.exports = router;
